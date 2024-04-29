9877eb6e2640ed5b98127a0a8bd0678ade78c497
Rabin <143919813+rawbeen10@users.noreply.github.com>
2024-04-29 18:19:54 +0545

    Create stringMethods.js
    this file contains different string  methods in javascript.

---
+++ stringMethods.js
@@ -0,0 +1,25 @@
+        let name = "Rabin"
+        //string.length()
+        console.log(name.length)
+
+        //string.toUpperCase()
+        console.log(name.toUpperCase())
+
+        //String.toLowerCase()
+        console.log(name.toLowerCase())
+
+        //string.slice()
+        console.log(name.slice(2,4))
+            
+        //string.replace()
+        let name1 = "Rabin Dhakal"
+        console.log(name1.replace("Dhakal", "Dada"))
+
+        //string.concat()        
+        let name2 = "Thor"
+        console.log(name.concat(" is friend of ", name2))
+        
+        //string.trim()
+        let friend = "   IronMan    "
+        console.log(friend)
+        console.log(friend.trim())
